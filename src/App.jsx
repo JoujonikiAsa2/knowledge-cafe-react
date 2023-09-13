df59a388a9ed57fe960f358b0e941e09ee989c8e
joujonikiasa2 <joujonikiasaroy@gmail.com>
2023-09-13 10:29:08 +0600

    All file added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
+import './App.css'
+
+function App() {
+  return (
+    <>      
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">Knolegde Cafe</h1>
+            {/* <div>
+                <img src="" alt="" />
+            </div> */}
+        </div>
+    );
+};
+
+export default Header;
